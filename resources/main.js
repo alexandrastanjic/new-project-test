83b0a06f268e3bab076a8cc2362791036be50079
Alexandra S <alexandrastanjic@gmail.com>
2020-11-08 22:57:46 +0200

    working on a new branch

--- resources/main.js
+++ resources/main.js
@@ -1,30 +1,30 @@
 $(function() {
-  console.log(products.coats);
-  const productsContentWrapper = $('.products-main-wrapper');
-  const singleProductWrappwe = $('.single-product-wrapper')
+  const productsContentWrapper = $('.product-main-wrapper');
+  const coatsArr = products.coats;
+  console.log(products);
+  const singleProductWrapper = $('.single-product-wrapper')
   const getproductsHTML = function(productsObj) {
-      return `<div class="single-product-wrapper" data-id=${productsObj['id']}>
-                <div class="product-image-wrapper" style="background-image: url(assets/products/${productsObj['imgUrl']})"></div>
+      return `<div class="single-product-wrapper" data-id=${productsObj.id}>
+                <div class="product-image-wrapper" style="background-image: url(assets/products/${productsObj.imgUrl})"></div>
                 <div class="product-info-wrapper">
-                    <div class="description-wrapper">${productsObj['name']}</div>
-                    <div class="price-wrapper">${productsObj['currency']} ${productsObj['price']}</div>
+                    <div class="description-wrapper">${productsObj.name}</div>
+                    <div class="price-wrapper">${productsObj.curency} ${productsObj.price}</div>
                </div> `;
   };
 
-  for(let i = 0; i < products['coats'].length; i++) {
-            let productsObj = products['coats'][i];
-            console.log(productsObj)
+  for(let i = 0; i < coatsArr.length; i++) {
+            let productsObj = coatsArr[i];
             let productsHMTL = getproductsHTML(productsObj);
-             productsContentWrapper.append(productsHMTL);
-        }
+                productsContentWrapper.append(productsHMTL);
+        }   
 
-        productsContentWrapper.delegate('.single-product-wrapper', "click", function() {
-          imgWrapper.css({backgroundImage: "url(assets/" + $(this).data('img') + ")"})
-                  overlay.fadeIn();
-              });
+        // productsContentWrapper.delegate('.single-product-wrapper', "click", function() {
+        //   imgWrapper.css({backgroundImage: "url(assets/" + $(this).data('img') + ")"})
+        //           overlay.fadeIn();
+        //       });
           
-              overlay.click(function() {
-                  $(this).fadeOut();
-              });
-          });
+        //       overlay.click(function() {
+        //           $(this).fadeOut();
+        //       });
+        //   });
 });
